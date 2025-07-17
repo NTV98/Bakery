86bb758a277ac788ea1863683a4541edb5c2add9
Nguyễn Tuấn Vũ <90400967+NTV98@users.noreply.github.com>
2025-07-17 21:27:58 +0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useTranslation } from '../i18n';
 
-
-//
 const AboutPage = () => {
   const { t } = useTranslation();
 
